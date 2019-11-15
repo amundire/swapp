4b29da126b48afb39b726d9eb68bc1a52b5d2d26
amundire <valentin.petrov420@abv.bg>
2019-11-15 20:51:19 +0200

    fixed, accidentally applied differences into a file

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
-<<<<<<< HEAD
 import { AUTH_MUTATION } from './api/requester'
-=======
-import AUTH_MUTATION from './api/requester'
->>>>>>> ca7144edf64098390eec36fd244d451e605fdd0e
 import Signin from './Signin';
 import Pages from './pages/index';
 import Header from './components/common/Header';
 import { Redirect } from 'react-router-dom';
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> ca7144edf64098390eec36fd244d451e605fdd0e
 const Home = () => {
     const isAuth = useQuery(AUTH_MUTATION).data;
 
@@ -28,11 +19,7 @@ const Home = () => {
     };
 
     React.useEffect(() => {
-<<<<<<< HEAD
         document.body.classList = currentTheme;
-=======
-        document.body.classList = theme;
->>>>>>> ca7144edf64098390eec36fd244d451e605fdd0e
     }, [currentTheme]);
 
     return isAuth.authenticated ?
